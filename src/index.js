7ca10d2fe9c5665ea844465a11bf5d9d2f34ab30
gperreymond <gperreymond@gmail.com>
2016-04-12 20:51:20 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
 'use strict'
 
+// load configurations
+const nconf = require("nconf")
+nconf.argv().env().file({ file: 'nconf-env.json' })
+
 const feathers = require('feathers')
 const bodyParser = require('body-parser')
-const handler = require('feathers-errors/handler')
+const error = require('feathers-errors/handler')
+const rest = require('feathers-rest')
 
 const r = require('rethinkdbdash')({
-  port: 29015,
-  host: 'place-with-no-firewall.com',
+  silent: true,
+  port: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_PORT'),
+  host: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_HOST'),
+  authKey: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_AUTH_KEY'),
+  db: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_DB')
 })
 const service = require('feathers-rethinkdb')
 
 // A Feathers app is the same as an Express app
 const app = feathers()
 
-// Parse HTTP JSON bodies
-app.use(bodyParser.json());
-// Parse URL-encoded params
-app.use(bodyParser.urlencoded({ extended: true }));
+app.configure(rest());
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/surveys', service({
+  Model: r,
+  name: 'surveys'
+}))
 
 // Register a nicer error handler than the default Express one
-app.use(handler())
+app.use(error())
 
 // Start the server
-app.listen(8080, '0.0.0.0', function () {
-  console.log(app);
+app.listen( nconf.get('ABIBAO_SERVICES_GATEWAY_EXPOSE_PORT'),  nconf.get('ABIBAO_SERVICES_GATEWAY_EXPOSE_IP'), function () {
+  console.log('server started')
 })
