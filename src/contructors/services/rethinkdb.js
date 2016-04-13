8dc987862554c738e70ae51dc94fe834453856a5
Gilles Perreymond <gperreymond@viadeoteam.com>
2016-04-13 17:50:22 +0200

    add hooks in services

---
+++ src/contructors/services/rethinkdb.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// load configurations
+const nconf = require('nconf')
+nconf.argv().env().file({ file: 'nconf-deve.json' })
+
+// rethinkdb connector
+const r = require('rethinkdbdash')({
+  silent: true,
+  port: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_PORT'),
+  host: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_HOST'),
+  authKey: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_AUTH_KEY'),
+  db: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_DB')
+})
+const Service = require('feathers-rethinkdb').Service
+
+module.exports.r = r
+module.exports.Service = Service
--- src/services/surveys.js
+++ src/services/surveys.js
@@ -7,36 +7,31 @@ nconf.argv().env().file({ file: 'nconf-deve.json' })
 // externals libraries
 const _ = require('lodash')
 
-// rethinkdb connector
-const r = require('rethinkdbdash')({
-  silent: true,
-  port: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_PORT'),
-  host: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_HOST'),
-  authKey: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_AUTH_KEY'),
-  db: nconf.get('ABIBAO_SERVICES_GATEWAY_SERVER_RETHINK_DB')
-})
-const Service = require('feathers-rethinkdb').Service
+// internals libraries
+const Rethinkdb = require('./../contructors/services/rethinkdb')
 
 // constructor class
-class Surveys extends Service {
-  find (params) {
-    super.find(params)
-      .then(function (data) {
-        console.log(data)
-        _.map(data, function (item) {
+class Surveys extends Rethinkdb.Service {
+  setup (app) {
+    app.service('surveys').before({
+      find(hook) {
+        console.log('surveys hook find before')
+      }
+    }),
+    app.service('surveys').after({
+      find(hook) {
+        console.log('surveys hook find after')
+        hook.result = _.map(hook.result, function (item) {
           delete item.id
+          return item
         })
-        return data
-      })
-      .catch(function (error) {
-        console.log(error)
-        return error
-      })
+      }
+    })
   }
 }
 
 // exports
 module.exports = new Surveys({
-  Model: r,
+  Model: Rethinkdb.r,
   name: 'surveys'
 })
